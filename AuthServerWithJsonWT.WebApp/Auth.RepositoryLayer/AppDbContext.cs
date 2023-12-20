616f1279a307f4916c134e15b0dfe82af5141045
Ender Bakır <enderyx_123@hotmail.com>
2023-12-21 01:12:03 +0300

    Tüm Katmanlarda Düzenlemeler Yapıldı
    Generic yapılar tekrar elden geçirildi(IServis - Servis, IGenericRepository - GenericRepository), Repository Design Pattern'de iyileştirmeler yapıldı. Context sınıfı revize edildi. Program.cs Configuration yapıları düzenlendi. NutritionController’ın View(index) sayfasına, diyetisyenlerin listelendiği tablo eklendi, Default/Index sayfasına linki verildi. Identity ile ilgili işlemler gözden geçirilerek, buglar fixlendi.

---
+++ AuthServerWithJsonWT.WebApp/Auth.RepositoryLayer/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Auth.CoreLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.RepositoryLayer
+{
+    public class AppDbContext :IdentityDbContext<UserApp , IdentityRole , string>
+    {
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                 
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> userRefreshTokens  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
