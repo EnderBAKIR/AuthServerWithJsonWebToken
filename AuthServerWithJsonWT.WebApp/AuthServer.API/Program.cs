20a76aa94a6a7a9fb1d5c24bed666f9598c9d228
Ender Bakır <enderyx_123@hotmail.com>
2024-01-02 13:41:54 +0300

    Update Program.cs
    containerlar program cs e eklendi , veri tabanı bağlantısı yapıldı.

--- AuthServerWithJsonWT.WebApp/AuthServer.API/Program.cs
+++ AuthServerWithJsonWT.WebApp/AuthServer.API/Program.cs
@@ -1,11 +1,43 @@
 using Auth.CoreLayer.Configuration;
+using Auth.CoreLayer.Models;
+using Auth.CoreLayer.Repositories;
+using Auth.CoreLayer.Services;
+using Auth.CoreLayer.UnitOfWork;
+using Auth.RepositoryLayer;
+using Auth.ServiceLayer.Services;
 using Auth.SharedLibrary.Configurations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Extensions Container
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(IGenericRepository<>));
+builder.Services.AddScoped(typeof(IService<,>), typeof(IService<,>));
+builder.Services.AddScoped<IUnitOfWork, IUnitOfWork>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly("Auth.RepositoryLayer");
+    });
+
+});
+builder.Services.AddIdentity<UserApp, IdentityRole>(Opt =>
+{
+    Opt.User.RequireUniqueEmail = true;
+    Opt.Password.RequireNonAlphanumeric = false;
+
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
